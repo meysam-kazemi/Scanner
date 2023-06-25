83ca97071638bff6d271b7b361114c2e88e795eb
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-25 09:28:38 +0330

    load the image and resize it

--- scan.py
+++ scan.py
@@ -1,5 +1,17 @@
+# =============================================================================
 # Import Libraries
+# =============================================================================
 import numpy as np
 import cv2
 import imutils
 import matplotlib.pyplot as plt
+
+# =============================================================================
+# Edge Detection
+# =============================================================================
+# load the image and compute the ratio of the old height to the new height, and resize it.
+img = cv2.imread("image.jpg")
+ratio = img.shape[0] / 500.0 # 500 : new height
+original = img.copy()
+img = imutils.resize(img,height=500) # resize the image
+
