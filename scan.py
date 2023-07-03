cc91c01e879047aaa00223275a0eb7731511f5d6
meysam-kazemi <meisam.daysu@gmail.com>
2023-07-03 18:36:38 +0330

    change the image

--- scan.py
+++ scan.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # Edge Detection
 # =============================================================================
 # load the image and compute the ratio of the old height to the new height, and resize it.
-img = cv2.imread("image.jpg")
+img = cv2.imread("img.jpeg")
 ratio = img.shape[0] / 500.0 # 500 : new height
 original = img.copy()
 img = imutils.resize(img,height=500) # resize the image
@@ -21,3 +21,10 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
 # Detecting edges
 edge = cv2.Canny(gray,75,200)  
+
+# Show image and its edges
+cv2.imshow("Original image",img)
+cv2.imshow("Edges",edge)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
