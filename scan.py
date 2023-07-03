e017ca5a361f122ea676eab47264f6f889a07869
meysam-kazemi <meisam.daysu@gmail.com>
2023-07-03 18:40:11 +0330

    finding contours

--- scan.py
+++ scan.py
@@ -20,11 +20,34 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # Blur image
 blur = cv2.GaussianBlur(gray,(5,5),0)
 # Detecting edges
-edge = cv2.Canny(gray,75,200)  
+edged = cv2.Canny(gray,75,200)  
 
-# Show image and its edges
-cv2.imshow("Original image",img)
-cv2.imshow("Edges",edge)
+# # Show image and its edges
+# cv2.imshow("Original image",img)
+# cv2.imshow("Edges",edged)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# =============================================================================
+# Finding Contours
+# =============================================================================
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
