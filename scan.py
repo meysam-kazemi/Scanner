56c21c9898ac881f6e2f0b1e668326e0eeebfc0b
meysam-kazemi <meisam.daysu@gmail.com>
2023-07-03 17:31:14 +0330

    detecting edges

--- scan.py
+++ scan.py
@@ -15,3 +15,9 @@ ratio = img.shape[0] / 500.0 # 500 : new height
 original = img.copy()
 img = imutils.resize(img,height=500) # resize the image
 
+# Convert to gray scale
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Blur image
+blur = cv2.GaussianBlur(gray,(5,5),0)
+# Detecting edges
+edge = cv2.Canny(gray,75,200)  
